4ea91d517b5eae8632b1377f2ab29c84ab741b8f
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-08-10 20:46:16 -0700

    done with the assignment

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+
+class Shoe 
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize (brand)
+        @brand = brand
+    end
+
+    def cobble 
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+shoe1 = Shoe.new("Tory Burch")
+shoe1.color = "Pink"
+shoe1.size = 6
+shoe1.condition = "new"
+
+puts shoe1.condition
